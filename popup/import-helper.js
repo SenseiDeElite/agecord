a95158d885788ad041adda5e01735e8964c9cbb7
Sensei 先生 <160349295+SenseiDeElite@users.noreply.github.com>
2026-07-29 16:08:32 -0400

    Add GPL-3.0 license notice to import-helper.js
    Added license information to the top of the file.

--- popup/import-helper.js
+++ popup/import-helper.js
@@ -1,3 +1,8 @@
+/*
+ * This source code is licensed under the GNU General Public License v3.0 (GPL-3.0).
+ * See the full license text: https://github.com/SenseiDeElite/agecord/blob/main/LICENSE
+ */
+
 'use strict';
 
 const statusEl = document.getElementById('status');
