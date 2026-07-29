7d17abf24f0072365de03d6e63a3e53a48ce4361
Sensei 先生 <160349295+SenseiDeElite@users.noreply.github.com>
2026-07-29 16:06:06 -0400

    Add GPL-3.0 license header to highlight_map.js
    Added license information at the top of highlight_map.js.

--- content/highlight_map.js
+++ content/highlight_map.js
@@ -1,3 +1,8 @@
+/*
+ * This source code is licensed under the GNU General Public License v3.0 (GPL-3.0).
+ * See the full license text: https://github.com/SenseiDeElite/agecord/blob/main/LICENSE
+ */
+
 // highlight_map.js
 // All valid highlight.js language aliases, used only to decide whether a
 // language header should be shown on a fenced code block.  No actual syntax
