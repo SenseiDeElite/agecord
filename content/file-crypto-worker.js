b20a27a443f562ec68b67808f8408a4a68465198
Sensei 先生 <160349295+SenseiDeElite@users.noreply.github.com>
2026-07-29 16:05:49 -0400

    Add GPL-3.0 license header to file-crypto-worker.js
    Added license information to the file.

--- content/file-crypto-worker.js
+++ content/file-crypto-worker.js
@@ -1,3 +1,8 @@
+/*
+ * This source code is licensed under the GNU General Public License v3.0 (GPL-3.0).
+ * See the full license text: https://github.com/SenseiDeElite/agecord/blob/main/LICENSE
+ */
+
 // file-crypto-worker.js — Dedicated Worker for file encryption and decryption
 //
 // File format (verify ops):
